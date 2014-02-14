2a851bd1a832b8fe505e0a8878275061ee34db4c
Boyoung Kwon <boyoung.kwon@gmail.com>
2014-02-14 14:09:24 -0500

    create student_scraper.rb

---
+++ lib/models/student_scraper.rb
@@ -0,0 +1,22 @@
+require 'nokogiri'
+require 'open-uri'
+
+
+class StudentScraper
+  def initialize(url)
+    @doc = Nokogiri::HTML(open(url))
+  end
+
+  def scrape
+    scrape_name
+    create_instance
+  end
+
+  def scrape_student
+    @name = @doc.search("div.big-comment h3 a").text
+  end
+
+end
+
+scrape = StudentScraper.new("http://students.flatironschool.com/")
+puts scrape.scrape_student
