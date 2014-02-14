f53304483e8cd63e4f46aa66def51b5406e154a6
wontaeyang <wontaeyang@gmail.com>
2014-02-14 18:16:06 -0500

    update scraper

--- lib/models/student_scraper.rb
+++ lib/models/student_scraper.rb
@@ -23,22 +23,14 @@ class StudentScraper
        excerpt = profile_doc.search("div.textwidget h3").text
        description = profile_doc.search("div.services p")[0].text
        github = profile_doc.search("div.coder-cred a")[0]['href']
-
-       @students << {:name => name, :excerpt => excerpt, :description => description}
+       indexPic = profile_doc.search("img.student_pic")[0]['src']
+       @students << {:name => name, :excerpt => excerpt, :description => description, :indexPic => indexPic, :url => profile}
     
     end
     puts @students
     
 
   end
-
-
-
-  # name = @doc.search("div.big-comment h3 a").text
-  # excerpt = @doc.search("div.blog-title p.home-blog-post-meta").text
-  # description = @doc.search("div.excerpt p").text
-  # htmlpics = @doc.search("div.blog-thumb a img")
-  # indexPic = htmlpics.collect{|imglink| imglink['src']}
 
 
 def create_instance
