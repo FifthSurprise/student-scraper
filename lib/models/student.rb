b9915c8a7afb1e6855139d36e73f376181fb6c2d
Kevin Chang <kevin.w.chang@gmail.com>
2014-02-14 14:34:55 -0500

    Updated database for new files

--- lib/models/student.rb
+++ lib/models/student.rb
@@ -1,10 +1,27 @@
 class Student
-  attr_accessor :name
+  attr_accessor :name, :excerpt, :description, :indexPic, :url
 
   def self.create_table
-    DB.execute("CREATE TABLE IF NOT EXISTS students (id integer primary key autoincrement, name text")
+    DB.execute("CREATE TABLE IF NOT EXISTS students (id integer primary key autoincrement,
+      name text, excerpt text, description text, indexPic text, url text)")
   end
   def save
-    DB.execute("Insert into students (name) values(?) where name=?",name)
+    #DB.execute("Insert into students (name) values(?) where name=?",name,excerpt,description,indexpic,url)
+    DB.execute("INSERT INTO student (name,excerpt,description,indexpic,url) values (:name, :excerpt, :description, :indexpic, :url)", to_h)
+
   end
+  def to_h
+    {
+      :name =>name,
+      :excerpt =>excerpt,
+      :description =>description,
+      :indexPic => indexPic,
+      :url => url,
+    }
+  end
+  # Name
+  # Excerpt
+  # Description
+  # IndexPic
+  # ProfileLink
 end
