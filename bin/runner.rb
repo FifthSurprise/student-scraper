79f25cbb4abb62d1deabb11c69f090745934d2c4
Kevin Chang <kevin.w.chang@gmail.com>
2014-02-14 16:21:14 -0500

    Added some testing code in anticipation of scraper

--- bin/runner.rb
+++ bin/runner.rb
@@ -1,4 +1,17 @@
 require_relative "../config/environment"
 
+#created the database & table
 DB = SQLite3::Database.new "students.db"
 Student.create_table
+
+#scrape all of the students off the page into an array
+scraper = StudentScraper.new('http://students.flatironschool.com')
+scrapedStudentsArray = scraper.scrape
+
+#create an array for Student objects
+studentObjectList = []
+
+#create students from the array of students
+scrapedStudentsArray.each do |studentHash|
+  studentObjectList.push(Student.new(studentHash))
+end
--- lib/models/student.rb
+++ lib/models/student.rb
@@ -1,14 +1,24 @@
 class Student
   attr_accessor :name, :excerpt, :description, :indexPic, :url
 
+  def initialize (studentHash)
+    @name = studentHash[:name]
+    @excerpt = studentHash[:excerpt]
+    @description = studentHash[:description]
+    @indexPic = studentHash[:indexPic]
+    @url = studentHash[:url]
+
+    save
+  end
+
   def self.create_table
     DB.execute("CREATE TABLE IF NOT EXISTS students (id integer primary key autoincrement,
       name text, excerpt text, description text, indexPic text, url text)")
   end
+
   def save
     DB.execute("INSERT INTO students (name,excerpt,description,indexpic,url) values(?,?,?,?,?)",
                @name,@excerpt,@description,@indexpic,@url)
-    #DB.execute("INSERT INTO students (name,excerpt,description,indexpic,url) values (:name, :excerpt, :description, :indexpic, :url)", to_h)
   end
   def to_h
     {
