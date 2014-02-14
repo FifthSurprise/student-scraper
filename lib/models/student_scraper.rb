eae3a9718776e61d07f76a5cdf55cc785bcadfd9
wontaeyang <wontaeyang@gmail.com>
2014-02-14 18:09:19 -0500

    fixed scrapers

--- lib/models/student_scraper.rb
+++ lib/models/student_scraper.rb
@@ -12,51 +12,46 @@ class StudentScraper
     create_instance
   end
 
-#   projects = {}
-# kickstarter.css("li.project.grid_4").each do |project|
-#   title = project.css("h2.bbcard_name strong a").text.to_sym
-#   projects[title] = {
-#     :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
-#     :description => project.css("p.bbcard_blurb").text,
-#     :location => project.css("ul.project-meta span.location-name").text,
-#     :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
-#   }
-
   def scrape_students
+    htmllink = @doc.search("div.blog-thumb a")
+    url = htmllink.collect{|profilelink| profilelink['href']}
+    @students = []
 
-    @students = [] 
+    url.each do |profile|
+      profile_doc = Nokogiri::HTML(open("http://students.flatironschool.com/#{profile}"))
+       name = profile_doc.search("h4.ib_main_header").text
+       excerpt = profile_doc.search("div.textwidget h3").text
+       description = profile_doc.search("div.services p")[0].text
+       github = profile_doc.search("div.coder-cred a")[0]['href']
 
-    profile_html = @doc.search("li.home-blog-post")  
-    profile_html.each do |profile|
-      puts profile.css ("div.big-comment h3 a")
-      # student = {}
-      # student[:name] = profile.css("div.big-comment h3 a").text
-# puts student
-
+       @students << {:name => name, :excerpt => excerpt, :description => description}
+    
     end
+    puts @students
+    
 
-
-
-    # name = @doc.search("div.big-comment h3 a").text
-    # excerpt = @doc.search("div.blog-title p.home-blog-post-meta").text
-    # description = @doc.search("div.excerpt p").text
-    # htmlpics = @doc.search("div.blog-thumb a img")
-    # indexPic = htmlpics.collect{|imglink| imglink['src']}
-    # htmllink = @doc.search("div.blog-thumb a")
-    # url = htmllink.collect{|profilelink| profilelink['href']}
   end
 
-  def create_instance
-    @student = Student.new
-    @student.name = @name
-    @student.excerpt = @excerpt
-    @student.description = @description
-    @student.indexPic = @indexPic
-    @student.url = @url
-    @student.save
-    @student
-  end
+
+
+  # name = @doc.search("div.big-comment h3 a").text
+  # excerpt = @doc.search("div.blog-title p.home-blog-post-meta").text
+  # description = @doc.search("div.excerpt p").text
+  # htmlpics = @doc.search("div.blog-thumb a img")
+  # indexPic = htmlpics.collect{|imglink| imglink['src']}
+
+
+def create_instance
+  @student = Student.new
+  @student.name = @name
+  @student.excerpt = @excerpt
+  @student.description = @description
+  @student.indexPic = @indexPic
+  @student.url = @url
+  @student.save
+  @student
+end
 end
 
 scraper = StudentScraper.new('http://students.flatironschool.com/')
-puts scraper.scrape_students
+scraper.scrape_students
