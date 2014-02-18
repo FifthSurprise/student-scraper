bd534192bb92f3c0a02176ddf3f6d2c3f86c16d3
Kevin Chang <kevin.w.chang@gmail.com>
2014-02-18 10:46:36 -0500

    Integrated scraper and converting into database

--- lib/models/student.rb
+++ lib/models/student.rb
@@ -4,7 +4,7 @@ class Student
   def initialize (studentHash)
     @name = studentHash[:name]
     @excerpt = studentHash[:excerpt]
-    @description = studentHash[:description]
+    @description = studentHash[:description].chomp
     @indexPic = studentHash[:indexPic]
     @url = studentHash[:url]
 
--- lib/models/student_scraper.rb
+++ lib/models/student_scraper.rb
@@ -19,31 +19,27 @@ class StudentScraper
 
     url.each do |profile|
       profile_doc = Nokogiri::HTML(open("http://students.flatironschool.com/#{profile}"))
-       name = profile_doc.search("h4.ib_main_header").text
-       excerpt = profile_doc.search("div.textwidget h3").text
-       description = profile_doc.search("div.services p")[0].text
-       github = profile_doc.search("div.coder-cred a")[0]['href']
-       indexPic = profile_doc.search("img.student_pic")[0]['src']
-       @students << {:name => name, :excerpt => excerpt, :description => description, :indexPic => indexPic, :url => profile}
-    
+      name = profile_doc.search("h4.ib_main_header").text
+      excerpt = profile_doc.search("div.textwidget h3").text
+      description = profile_doc.search("div.services p")[0].text
+      github = profile_doc.search("div.coder-cred a")[0]['href']
+      indexPic = profile_doc.search("img.student_pic")[0]['src']
+      @students << {:name => name, :excerpt => excerpt, :description => description, :indexPic => indexPic, :url => profile}
+
     end
-    puts @students
-    
-
+    @students
   end
-
-
-def create_instance
-  @student = Student.new
-  @student.name = @name
-  @student.excerpt = @excerpt
-  @student.description = @description
-  @student.indexPic = @indexPic
-  @student.url = @url
-  @student.save
-  @student
-end
+  # def create_instance
+  #   @student = Student.new
+  #   @student.name = @name
+  #   @student.excerpt = @excerpt
+  #   @student.description = @description
+  #   @student.indexPic = @indexPic
+  #   @student.url = @url
+  #   @student.save
+  #   @student
+  # end
 end
 
-scraper = StudentScraper.new('http://students.flatironschool.com/')
-scraper.scrape_students
+# scraper = StudentScraper.new('http://students.flatironschool.com/')
+# scraper.scrape_students
